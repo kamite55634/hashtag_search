ab3716fec46b00c8f7cfadcc8551378966be82eb
kamite55634 <kamite.cocorozashi@gmail.com>
2019-12-23 15:52:00 +0900

    AND検索できるようにしました

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,16 +4,22 @@ class Submission < ApplicationRecord
 
   def self.search(search)
     if search && search != ""
-      hashtags = []
+      submissions_first = []
       words = search.split(/[[:blank:]]+/)
       words.each do |word|
         hashtag = Hashtag.find_by("name like ? ", "%#{word}%")
-        if hashtag
-          hashtags.push(hashtag.submissions)
+        if hashtag && submissions_first == []
+          submissions_first.push(hashtag.submissions)
+
+        elsif hashtag
+          submissions_second = []
+          submissions_second.push(hashtag.submissions)
+          submissions_first = submissions_second.flatten & submissions_first.flatten
         end
+
       end
       $notice = "#{words}の検索結果を表示しています"
-      return hashtags.flatten.uniq!
+      return submissions_first.flatten
     else
       $notice = "全ての投稿を表示しています"
       return Submission.all
